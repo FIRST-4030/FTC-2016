a0d8497825ce4c2c3fd0f5ec84b96919019744d2
Tesla Knight <abtesla@gmail.com>
2016-09-30 17:10:11 -0700

    Imported TankOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankOpMode.java
@@ -35,6 +35,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * This file contains an example of an iterative (Non-Linear) "OpMode".
@@ -42,23 +45,44 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * The names of OpModes appear on the menu of the FTC Driver Station.
  * When an selection is made from the menu, the corresponding OpMode
  * class is instantiated on the Robot Controller and executed.
- *
+ * <p>
  * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
  * It includes all the skeletal structure that all iterative OpModes contain.
- *
+ * <p>
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Template: Iterative OpMode", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Template: Iterative OpMode", group = "Iterative Opmode")
+// @Autonomous(...) is the other common choice
 @Disabled
-public class TeleOp extends OpMode
-{
+
+public class TankOpMode extends OpMode {
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
+    protected DcMotor frontLeftMotor;
+    protected DcMotor frontRightMotor;
+    protected DcMotor backLeftMotor;
+    protected DcMotor backRightMotor;
+    protected boolean hasTwoMotors;
 
-    // private DcMotor leftMotor = null;
-    // private DcMotor rightMotor = null;
+
+    public TankOpMode(DcMotor lMotorName, DcMotor rMotorName) {
+
+        frontLeftMotor = lMotorName;
+        frontRightMotor = rMotorName;
+        hasTwoMotors = true;
+
+    }
+
+    public TankOpMode(DcMotor fLMotor, DcMotor fRMotor, DcMotor bLMotor, DcMotor bRMotor) {
+
+        frontLeftMotor = fLMotor;
+        frontRightMotor = fRMotor;
+        backLeftMotor = bLMotor;
+        backRightMotor = bRMotor;
+        hasTwoMotors = false;
+    }
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -66,46 +90,76 @@ public class TeleOp extends OpMode
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
-
+        frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+        if (!hasTwoMotors) {
+            backRightMotor.setDirection(DcMotor.Direction.REVERSE);
         /* eg: Initialize the hardware variables. Note that the strings used here as parameters
          * to 'get' must correspond to the names assigned during the robot configuration
          * step (using the FTC Robot Controller app on the phone).
          */
-        // leftMotor  = hardwareMap.dcMotor.get("left motor");
-        // rightMotor = hardwareMap.dcMotor.get("right motor");
+            // leftMotor  = hardwareMap.dcMotor.get("left motor");
+            // rightMotor = hardwareMap.dcMotor.get("right motor");
 
-        // eg: Set the drive motor directions:
-        // Reverse the motor that runs backwards when connected directly to the battery
-        // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        //  rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-        // telemetry.addData("Status", "Initialized");
+            // eg: Set the drive motor directions:
+            // Reverse the motor that runs backwards when connected directly to the battery
+            // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+            //  rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+            // telemetry.addData("Status", "Initialized");
+        }
     }
 
     /*
      * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
      */
-    @Override
-    public void init_loop() {
-    }
+        @Override
+        public void init_loop() {
+        }
 
     /*
      * Code to run ONCE when the driver hits PLAY
      */
-    @Override
-    public void start() {
-        runtime.reset();
-    }
+        @Override
+        public void start() {
+            runtime.reset();
+        }
 
     /*
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
      */
-    @Override
-    public void loop() {
-        telemetry.addData("Status", "Running: " + runtime.toString());
+        @Override
+        public void loop () {
+            telemetry.addData("Status", "Running: " + runtime.toString());
+            CollectTelemetry();
 
-        // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
-        // leftMotor.setPower(-gamepad1.left_stick_y);
-        // rightMotor.setPower(-gamepad1.right_stick_y);
+            float left = gamepad1.left_stick_y;
+            float right = gamepad1.right_stick_y;
+            right = moderateMotorPower(Range.clip(right, -1f, 1f));
+            left = moderateMotorPower(Range.clip(left, -1f, 1f));
+            frontRightMotor.setPower(right);
+            frontLeftMotor.setPower(left);
+            if (!hasTwoMotors) {
+                backRightMotor.setPower(right);
+                backLeftMotor.setPower(left);
+
+                // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
+                // leftMotor.setPower(-gamepad1.left_stick_y);
+                // rightMotor.setPower(-gamepad1.right_stick_y);
+            }
+        }
+
+    public float moderateMotorPower(float motorPower) {
+
+        if (motorPower < 0.1 && motorPower > -0.1) {
+
+            return 0;
+        } else {
+            return motorPower;
+        }
+    }
+
+    protected void CollectTelemetry() {
+        telemetry.addData("G1LY", gamepad1.left_stick_y);
+        telemetry.addData("G1RY", gamepad1.right_stick_y);
     }
 
     /*
